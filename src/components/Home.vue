0e826cd2c3a3acb0881b00e275d9e4acef99ae5c
Pedro Henrique Gonçalo de Oliveira <pedro.gonsalo939@gmail.com>
2025-01-04 13:22:35 -0300

    Retirado o vuetify do projeto (mudei de ideia e vou utilizar o datagrid da DevExpress)

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,5 +5,44 @@
 <script>
 export default {
   name: 'HomePage',
+  data() {
+    return {
+      headers: [
+        { text: 'ID', value: 'id' },
+        { text: 'Nome', value: 'nome' },
+        { text: 'Email', value: 'email' },
+        { text: 'Senha', value: 'senha' },
+        { text: 'Data de Criação', value: 'data_criacao' },
+        { text: 'Data de Atualização', value: 'data_atualizacao' }
+      ],
+      items: [
+        {
+          id: 1,
+          nome: 'Novo Nome',
+          email: 'teste@email.com',
+          senha: 'senha123',
+          data_criacao: '2025-01-03T23:57:32.225Z',
+          data_atualizacao: '2025-01-03T23:57:32.225Z'
+        }
+      ]
+    }
+  },
+  methods: {
+    AddUser() {
+
+    },
+
+    EditUser(item) {
+      console.log(item);
+    },
+
+    DeleteUser(item) { 
+      console.log(item);
+    }
+  }
 }
 </script>
+
+<style scoped>
+
+</style>
