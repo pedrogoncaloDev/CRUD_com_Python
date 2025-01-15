2c5ab2a815de5ce1fbc381f850e13c1b05abd574
Pedro Henrique Gonçalo de Oliveira <pedro.gonsalo939@gmail.com>
2025-01-14 21:27:31 -0300

    Criação da tabela de usuários no front-end

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,40 @@
 <template>
-  <v-text-field class="mx-3" label="Pesquisar" clearable></v-text-field>
+  <div>
+    <v-text-field class="mx-3" label="Pesquisar" clearable></v-text-field>
+
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      class="elevation-1 mx-3"
+    >
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Users</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" @click="AddUser">Add User</v-btn>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.action="{ item }">
+        <v-btn icon @click="EditUser(item)">
+          <v-icon>mdi-pencil</v-icon>
+        </v-btn>
+        <v-btn icon @click="DeleteUser(item)">
+          <v-icon>mdi-delete</v-icon>
+        </v-btn>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'HomePage',
+
   data() {
     return {
       headers: [
+        { text: 'Actions', value: 'action', sortable: false },
         { text: 'ID', value: 'id' },
         { text: 'Nome', value: 'nome' },
         { text: 'Email', value: 'email' },
@@ -29,20 +56,20 @@ export default {
   },
   methods: {
     AddUser() {
-
+      // Lógica para adicionar um novo usuário
     },
-
     EditUser(item) {
-      console.log(item);
+      console.log('Edit item:', item);
+      // Lógica para editar o usuário
     },
-
-    DeleteUser(item) { 
-      console.log(item);
+    DeleteUser(item) {
+      console.log('Delete item:', item);
+      // Lógica para deletar o usuário
     }
   }
 }
 </script>
 
 <style scoped>
-
+/* Estilos personalizados */
 </style>
