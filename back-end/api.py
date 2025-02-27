e0a260fc737932e4314a30ac409000483e2eaab7
Pedro-Gonsalo <pedro.gonsalo939@gmail.com>
2025-02-27 08:58:23 -0300

    Rota para criar usuários (CREATED) finalizada

--- back-end/api.py
+++ back-end/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from users import Users
 from config_db import conn_info
 from utils import date_to_string
@@ -15,23 +15,34 @@ def read_users():
 
     return json_response, 200 
 
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     user_data = request.json
-#     users.create_user(user_data)
-#     return jsonify({"message": "Usuário criado com sucesso"}), 201
+@app.route('/users', methods=['POST'])
+def create_user():
+    try:
+        json_requisition = request.json
+        new_user = json.loads(json_requisition['query'])
+        user_created = users.create_user(new_user)
 
-# @app.route('/users/<int:user_id>', methods=['PUT'])
-# def update_user(user_id):
-#     user_data = request.json
-#     user_data['id'] = user_id
-#     users.update_user(user_data)
-#     return jsonify({"message": "Usuário atualizado com sucesso"}), 200
+        if user_created['success']:
+            return jsonify({"message": user_created["message"]}), 201
+        else:
+            return jsonify({"error": user_created["message"]}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
-# @app.route('/users/<int:user_id>', methods=['DELETE'])
-# def delete_user(user_id):
-#     users.delete_user(user_id)
-#     return jsonify({"message": "Usuário deletado com sucesso"}), 200
+
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    user_data = request.json
+    user_data['id'] = user_id
+    users.update_user(user_data)
+    return jsonify({"message": "Usuário atualizado com sucesso"}), 200
+
+
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    users.delete_user(user_id)
+    return jsonify({"message": "Usuário deletado com sucesso"}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- back-end/users.py
+++ back-end/users.py
@@ -1,5 +1,6 @@
 import psycopg2
 from psycopg2 import sql
+from datetime import datetime
 
 class Users:
     def __init__(self, conn_info):
@@ -9,14 +10,31 @@ class Users:
         return psycopg2.connect(**self.conn_info)
     
     def create_user(self, user):
-        with self.connect() as conn:
-            with conn.cursor() as cur:
-                cur.execute("""
-                    INSERT INTO usuarios (nome, email, senha, data_criacao, data_atualizacao)
-                    VALUES (%s, %s, %s, %s, %s) RETURNING id;
-                """, (user['nome'], user['email'], user['senha'], user['data_criacao'], user['data_atualizacao']))
-                new_id = cur.fetchone()[0]
-                print(f"Usuário inserido com ID: {new_id}")
+        try:
+            # Verifique se o email já existe
+            with self.connect() as conn:
+                with conn.cursor() as cur:
+                    cur.execute("SELECT id FROM usuarios WHERE email = %s", (user['email'],))
+                    if cur.fetchone():
+                        return {"success": False, "message": "Email já existe."}
+
+            
+            # Converta as strings de data para objetos datetime
+            user['data_criacao'] = datetime.fromisoformat(user['data_criacao'].replace('Z', '+00:00'))
+            user['data_atualizacao'] = datetime.fromisoformat(user['data_atualizacao'].replace('Z', '+00:00'))
+            
+            # Insira o usuário no banco de dados
+            with self.connect() as conn:
+                with conn.cursor() as cur:
+                    cur.execute("""
+                        INSERT INTO usuarios (nome, email, senha, data_criacao, data_atualizacao)
+                        VALUES (%s, %s, %s, %s, %s) RETURNING id;
+                    """, (user['nome'], user['email'], user['senha'], user['data_criacao'], user['data_atualizacao']))
+                    new_id = cur.fetchone()[0]
+
+                    return {"success": True, "message": f"Usuário inserido com ID: {new_id}"}
+        except Exception as e:
+            return {"success": False, "message": f"{e}"}
 
     def read_users(self):
         with self.connect() as conn:
