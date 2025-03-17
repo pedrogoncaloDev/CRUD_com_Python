f01cd73eb0cfeb0419772fbfb35c392a7170ec5a
Pedro-Gonsalo <pedro.gonsalo939@gmail.com>
2025-03-17 15:54:12 -0300

    Adicionado os segundos na function formatDate

--- src/utils.js
+++ src/utils.js
@@ -8,6 +8,7 @@ export function formatDate(isoString) {
     const year = date.getFullYear();
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
 
-    return `${day}/${month}/${year} ${hours}:${minutes}`;
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
 }
