961aae4e5dc59984d91949e17a6a597be64f7595
Pedro-Gonsalo <pedro.gonsalo939@gmail.com>
2025-03-18 16:37:51 -0300

    responsividade adicionada

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,55 +1,92 @@
 <template>
   <v-container fluid>
-    <v-text-field class="mx-3" label="Pesquisar" clearable></v-text-field>
+    <v-text-field
+      class="mx-3"
+      label="Pesquisar"
+      clearable
+    ></v-text-field>
 
     <v-data-table
       style="height: 100%; width: 100%;"
-      :headers="headers" no-data-text="Nenhum usuário cadastrado" :items="users" class="border-b"
-      items-per-page="10">
+      :headers="headers"
+      no-data-text="Nenhum usuário cadastrado"
+      :items="users"
+      class="border-b"
+      items-per-page="10"
+    >
       <template v-slot:no-data>
         <v-card-text>
-          <v-icon color="black" class=" mb-1" icon="mdi-emoticon-cool"></v-icon>
           Nenhum usuário cadastrado
         </v-card-text>
       </template>
       <template v-slot:item="{ item }">
         <tr>
-          <td>
-            <v-icon @click="EditUser(item)" icon="mdi mdi-file-edit-outline" size="30" class="mr-2" style="cursor: pointer;"></v-icon>
-            <v-icon @click="DeleteUser(item.id)" icon="mdi mdi-delete-outline" size="30" style="cursor: pointer;"></v-icon>
+          <td class="w-30 text-nowrap">
+            <v-icon
+              @click="EditUser(item)"
+              icon="mdi mdi-file-edit-outline"
+              size="30"
+              class="mr-2"
+              style="cursor: pointer;"
+            ></v-icon>
+            <v-icon
+              @click="DeleteUser(item.id)"
+              icon="mdi mdi-delete-outline"
+              size="30"
+              style="cursor: pointer;"
+            ></v-icon>
           </td>
-          <td>{{ item.id }}</td>
-          <td>{{ item.nome }}</td>
-          <td>{{ item.email }}</td>
-          <td>{{ item.senha }}</td>
-          <td>{{ formatDate(item.data_criacao) }}</td>
-          <td>{{ formatDate(item.data_atualizacao) }}</td>
+          <td class="w-30 text-nowrap">{{ item.id }}</td>
+          <td class="w-150 text-nowrap">{{ item.nome }}</td>
+          <td class="w-200 text-nowrap">{{ item.email }}</td>
+          <td class="w-150 text-nowrap">{{ item.senha }}</td>
+          <td class="w-200 text-nowrap">
+            {{ formatDate(item.data_criacao) }}
+          </td>
+          <td class="w-200 text-nowrap">
+            {{ formatDate(item.data_atualizacao) }}
+          </td>
         </tr>
       </template>
     </v-data-table>
 
-    <v-btn color="primary" @click="ShowModalAddUser = true">Cadastrar Novo Usuário</v-btn>
+    <v-btn
+      color="primary"
+      class="mt-3"
+      block
+      @click="ShowModalAddUser = true"
+    >
+      Cadastrar Novo Usuário
+    </v-btn>
   </v-container>
 
   <AddUserModal :dialog="ShowModalAddUser" @CloseModal="CloseModal" />
-  <EditUserModal :dialog="ShowModalEditUser" :informationsUser="informationsUser" @CloseModal="CloseModal" />
-  <DeleteUserModal :dialog="ShowModalDeleteUser" :id_user="IDUserDelete" @CloseModal="CloseModal" />
+  <EditUserModal
+    :dialog="ShowModalEditUser"
+    :informationsUser="informationsUser"
+    @CloseModal="CloseModal"
+  />
+  <DeleteUserModal
+    :dialog="ShowModalDeleteUser"
+    :id_user="IDUserDelete"
+    @CloseModal="CloseModal"
+  />
 </template>
 
 <script>
-import AddUserModal from '@/modais/AddUserModal.vue';
-import EditUserModal from '@/modais/EditUserModal.vue';
-import DeleteUserModal from '@/modais/DeleteUserModal.vue';
-import { API_URL, formatDate } from '../utils';
-import axios from 'axios';
+import AddUserModal from "@/modais/AddUserModal.vue";
+import EditUserModal from "@/modais/EditUserModal.vue";
+import DeleteUserModal from "@/modais/DeleteUserModal.vue";
+import { API_URL, formatDate } from "../utils";
+import axios from "axios";
 
 export default {
-  name: 'HomePage',
+  name: "HomePage",
 
   components: {
     AddUserModal,
     EditUserModal,
-    DeleteUserModal
+    DeleteUserModal,
   },
 
   data() {
@@ -64,14 +101,13 @@ export default {
         { title: 'Data de Atualização', key: 'data_atualizacao' },
       ],
 
-      informationsUser: {},
-
       users: [],
+      informationsUser: {},
       ShowModalAddUser: false,
       ShowModalEditUser: false,
       ShowModalDeleteUser: false,
-      IDUserDelete: null
-    }
+      IDUserDelete: null,
+    };
   },
 
   created() {
@@ -91,29 +127,32 @@ export default {
       this.GetUsers();
     },
 
-    GetUsers(){
-      axios.get(API_URL)
-      .then(response => {
-        this.users = response.data;
-      })
-      .catch(error => {
-        console.error("Houve um erro ao buscar os usuários:", error);
-      });
+    GetUsers() {
+      axios
+        .get(API_URL)
+        .then((response) => {
+          this.users = response.data;
+        })
+        .catch((error) => {
+          console.error("Houve um erro ao buscar os usuários:", error);
+        });
     },
 
-    EditUser(user){
-      this.informationsUser = user; 
+    EditUser(user) {
+      this.informationsUser = user;
       this.ShowModalEditUser = true;
     },
 
-    DeleteUser(id_user){
+    DeleteUser(id_user) {
       this.IDUserDelete = id_user;
       this.ShowModalDeleteUser = true;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
-
+.text-nowrap {
+  white-space: nowrap;
+}
 </style>
