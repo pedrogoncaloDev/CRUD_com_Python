b17c5ad4fa318bb9b24e2d3facac81ebb1649be6
Pedro-Gonsalo <pedro.gonsalo939@gmail.com>
2025-04-03 16:08:20 -0300

    trocando hashing por criptografia na senha ao cadastrar usuário

--- back-end/users.py
+++ back-end/users.py
@@ -2,7 +2,10 @@ import psycopg2
 from datetime import datetime
 from utils import is_valid_email
 import pytz 
-from bcrypt import hashpw, checkpw, gensalt
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
 
 class Users:
     def __init__(self, conn_database_crud_com_python):
@@ -24,14 +27,14 @@ class Users:
                     if cur.fetchone():
                         return {"success": False, "message": "Email já utilizado por outro usuário."}
                     
-                    hashed_password = hashpw(user_data['senha'].encode('utf-8'), gensalt())
+                    encrypt_password = self.encrypt_password(user_data['senha'])
                     data_now = datetime.utcnow().replace(tzinfo=pytz.UTC)
             
                     cur.execute("""
                                     INSERT INTO usuarios (nome, email, senha, data_criacao, data_atualizacao)
                                     VALUES (%s, %s, %s, %s, %s) RETURNING id;
                                 """, 
-                                (user_data['nome'], user_data['email'], hashed_password, data_now, data_now))
+                                (user_data['nome'], user_data['email'], encrypt_password, data_now, data_now))
                     new_id = cur.fetchone()[0]
 
                     return {"success": True, "message": f"Usuário inserido com ID: {new_id}"}
@@ -47,6 +50,9 @@ class Users:
 
                     columns = [desc[0] for desc in cur.description]  # Obtém os nomes das colunas
                     users = [dict(zip(columns, line)) for line in cur.fetchall()]  # Converte cada linha em um dicionário
+
+                    for user in users:
+                        user['senha'] = self.decrypt_password(user['senha'])
 
                     return {"success": True, "message": users}
         except Exception as e:
@@ -108,3 +114,11 @@ class Users:
 
         # Se todas as validações passarem
         return {"success": True, "message": "Validação bem-sucedida."}
+    
+
+    def encrypt_password(self, plain_password):
+        return cipher_suite.encrypt(plain_password.encode('utf-8')).decode('utf-8')
+    
+
+    def decrypt_password(self, encrypted_password):
+        return cipher_suite.decrypt(encrypted_password.encode('utf-8')).decode('utf-8')
