62cc9c3621eee5535267b072bdd8912499d4a261
Pedro-Gonsalo <pedro.gonsalo939@gmail.com>
2025-06-04 20:28:47 -0300

    alterando a estrutura de pastas do projeto

---
+++ back-end/config/config_db.py
@@ -0,0 +1,15 @@
+conn_database_crud_com_python = {
+    "dbname": "crud_com_python",
+    "user":"postgres",
+    "password": "masterkey",
+    "host": "localhost",
+    "port": 5432
+}
+
+conn_info = {
+    "dbname": "postgres",
+    "user": "postgres",
+    "password": "masterkey",
+    "host": "localhost",
+    "port": 5432, 
+}
