7d6fb9f7ce09855a22978134ff82dc7a7bd6efe4
Pedro-Gonsalo <pedro.gonsalo939@gmail.com>
2025-06-25 22:04:12 -0300

    fix: corrigindo a configuração do jest

--- front-end/jest.config.js
+++ front-end/jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-    testEnvironment: 'node',
-    testMatch: ['**/__tests__/**/*.js(*)', '**/?(*.)+(spec|test).js(x)'],
+    moduleFileExtensions: ['js', 'json', 'vue'], // extensões reconhecidas
     transform: {
-        '^.+\\.js$': ['babel-jest', { configFile: false }]
+        '^.+\\.vue$': '@vue/vue3-jest', // transforma arquivos .vue usando Vue 3 Jest
+        '^.+\\.(js|jsx)$': 'babel-jest' // transpila arquivos .js e .jsx usando Babel
     },
+    transformIgnorePatterns: [
+        '/node_modules/(?!(vuetify)/)' // transpila também o Vuetify (que usa import/export ES6)
+    ],
 }
