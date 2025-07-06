1b073c48fcbf7e2750853279ca7da0a08e519e2c
Pedro-Gonsalo <pedro.gonsalo939@gmail.com>
2025-07-06 14:18:26 -0300

    fix: corrige referências de conexão no teste de configuração do banco de dados

--- back-end/tests/test_config_db.py
+++ back-end/tests/test_config_db.py
@@ -2,10 +2,10 @@ from database.config_db import CONN_DATABASE_CRUD_COM_PYTHON, CONN_INFO
 
 def test_conn_database_crud_com_python_structure():
     expected_keys = ["dbname", "user", "password", "host", "port"]
-    assert set(config_db.CONN_DATABASE_CRUD_COM_PYTHON.keys()) == expected_keys
+    assert set(CONN_DATABASE_CRUD_COM_PYTHON.keys()) == set(expected_keys)
 
 def test_conn_database_crud_com_python_values():
-    conn = config_db.CONN_DATABASE_CRUD_COM_PYTHON
+    conn = CONN_DATABASE_CRUD_COM_PYTHON
     assert conn["dbname"] == "crud_com_python"
     assert conn["user"] == "postgres"
     assert conn["password"] == "masterkey"
@@ -14,10 +14,10 @@ def test_conn_database_crud_com_python_values():
 
 def test_conn_info_structure():
     expected_keys = ["dbname", "user", "password", "host", "port"]
-    assert set(config_db.CONN_INFO.keys()) == expected_keys
+    assert set(CONN_INFO.keys()) == set(expected_keys)
 
 def test_conn_info_values():
-    conn = config_db.CONN_INFO
+    conn = CONN_INFO
     assert conn["dbname"] == "postgres"
     assert conn["user"] == "postgres"
     assert conn["password"] == "masterkey"
