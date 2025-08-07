1408e1071b5400d8c51997b71805d62e90e999c9
PedroGoncaloGRV <pedro.henrique@grvsoftware.com.br>
2025-08-07 18:49:35 -0300

    feat: adicionando linhas do "host": "localhost"

--- back-end/database/config_db.py
+++ back-end/database/config_db.py
@@ -2,7 +2,8 @@ CONN_DATABASE_CRUD_COM_PYTHON  = {
     "dbname": "crud_com_python",
     "user":"postgres",
     "password": "masterkey",
-    "host": "host.docker.internal",
+    # "host": "host.docker.internal",
+    "host": "localhost",
     "port": 5432
 }
 
@@ -10,7 +11,8 @@ CONN_DATABASE_TEST_CRUD_COM_PYTHON  = {
     "dbname": "test_crud_com_python",
     "user":"postgres",
     "password": "masterkey",
-    "host": "host.docker.internal",
+    # "host": "host.docker.internal",
+    "host": "localhost",
     "port": 5432
 }
 
@@ -18,6 +20,7 @@ CONN_INFO = {
     "dbname": "postgres",
     "user": "postgres",
     "password": "masterkey",
-    "host": "host.docker.internal",
+    # "host": "host.docker.internal",
+    "host": "localhost",
     "port": 5432, 
 }
